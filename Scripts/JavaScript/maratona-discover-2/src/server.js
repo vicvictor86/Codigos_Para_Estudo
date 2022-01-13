fb571edede3b3ea22df6623028db816401652ddf
Victor <vicbem@hotmail.com>
2022-01-13 20:05:13 -0300

    Segunda aula da maratona discover concluida

--- Scripts/JavaScript/maratona-discover-2/src/server.js
+++ Scripts/JavaScript/maratona-discover-2/src/server.js
@@ -5,7 +5,9 @@ const routes = require('./routes');
 const port = 3000;
 server.set('view engine', 'ejs');
 
+//Set de configurações do servidor
 server.use(express.static("public"));
+server.use(express.urlencoded({extended: true}));
 server.use(routes);
 
 server.listen(port, () => console.log('Server rodando'));
