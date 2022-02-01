37a1a9e3aaa4bfd77b1f2616a24305bf4ddb2f72
Victor <vicbem@hotmail.com>
2022-02-01 16:56:46 -0300

    Módulo de react da gostack terminado

--- Scripts/React/frontend/src/App.js
+++ Scripts/React/frontend/src/App.js
@@ -1,24 +1,36 @@
-import React, { useState } from 'react';
-import Header from './components/Header';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import "./App.css";
-import backgroundImage from "./assets/background.jpg"
+
+import Header from './components/Header';
 
 export default function App(){
-    const [projects, setProjects] = useState(["React", "Front End"]);
+    const [projects, setProjects] = useState([]);
 
-    function handleAddProject(){
-        setProjects([...projects, `Novo projeto ${Date.now()}`]);
-        console.log(projects);
+    useEffect(() => {
+        api.get("projects").then(response => {
+            setProjects(response.data);
+        });
+    }, []);
+
+    async function handleAddProject(){
+        const response = await api.post("projects", {
+            title: `Novo projeto ${Date.now()}`,
+            owner: "Victor"
+        });
+
+        const project = response.data;
+        
+        setProjects([...projects, project]);
     }
     
     return (
         <>
             <Header title="Projects" >
-                <img width={300} src={backgroundImage} />
-
+                
                 <ul>
-                    {projects.map(project => <li key={project}>{project}</li>)}
+                    {projects.map(project => <li key={project.id}>{project.title}</li>)}
                 </ul>
 
                 <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
